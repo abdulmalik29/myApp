7ad0f27b8aa2cae412350a95328205babe73a0ee
abdulmalik alghazi <malka2974@gmail.com>
2020-07-13 19:27:18 +0400

    starting the project

---
+++ app/src/main/java/com/example/myapp/rightActivity.java
@@ -0,0 +1,16 @@
+package com.example.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class rightActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_right);
+    }
+}
