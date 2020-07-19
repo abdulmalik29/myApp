e0e930db7e0e4f8c266040bd7f291d55bb2a674b
abdulmalik alghazi <malka2974@gmail.com>
2020-07-19 23:06:01 +0400

    still working on login Activity

--- app/src/main/java/com/example/myapp/loginActivity.java
+++ app/src/main/java/com/example/myapp/loginActivity.java
@@ -20,13 +20,4 @@ public class loginActivity extends AppCompatActivity
         passwordEditText = findViewById(R.id.etPassword);
         userNameEditText = findViewById(R.id.etUserName);
     }
-    public void onLoginClick(View view)
-    {
-        String password = passwordEditText.getText().toString(); // gets the password in a variable
-        String username = userNameEditText.getText().toString();
-        String type = "login";
-
-        BackgroundWorker backgroundWorker = new BackgroundWorker(this);
-        backgroundWorker.execute(type, username, password);
-    }
 }
