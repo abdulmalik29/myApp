88ae34b0af98f29ef2082cca8c2fba2fc5c6542f
abdulmalik alghazi <malka2974@gmail.com>
2020-07-22 17:54:19 +0400

    still working on loginActivity and registerActivity

---
+++ app/src/main/java/com/example/myapp/PrefConfig.java
@@ -0,0 +1,18 @@
+package com.example.myapp;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class PrefConfig
+{
+    private SharedPreferences sharedPreferences;
+    private Context context;
+
+    public PrefConfig(Context context)
+    {
+        this.context = context;
+        sharedPreferences = context.getSharedPreferences(context.getString(R.string.pref_file),Context.MODE_PRIVATE);
+    }
+
+
+}
--- app/src/main/java/com/example/myapp/loginActivity.java
+++ app/src/main/java/com/example/myapp/loginActivity.java
@@ -2,15 +2,25 @@ package com.example.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class loginActivity extends AppCompatActivity
 {
-    EditText passwordEditText, userNameEditText;
-    Button logingButton;
+    private EditText passwordEditText, userNameEditText;
+    private Button loginButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
