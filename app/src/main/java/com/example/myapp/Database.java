a27713bfbccad6064a55eeea2ac70b439b52b8ed
abdulmalik alghazi <malka2974@gmail.com>
2020-08-04 22:30:41 +0400

    quality of life updates 3

--- app/src/main/java/com/example/myapp/Database.java
+++ app/src/main/java/com/example/myapp/Database.java
@@ -1,10 +1,14 @@
 package com.example.myapp;
 
 import androidx.room.Dao;
+import androidx.room.PrimaryKey;
 import androidx.room.RoomDatabase;
 
 @androidx.room.Database(entities = {User.class}, version = 1)
 public abstract class Database extends RoomDatabase
 {
     public abstract DAO dao();
+
+    private static volatile int i;
+
 }
